90a3e273c1a3d0cf263c5eb9dc0a98e6cfbb0987
Igor Vinoski <igor.vinoki2@gmail.com>
2025-02-28 08:35:31 -0300

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Titulo from './titulo';
+function App() {
+  return (
+      <><h1>Seja bem vindo ao React </h1><Titulo /></>
+  );
+}
+
+export default App;
---
+++ src/titulo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Titulo() {
+    return (
+        <div>
+            <h1>Meu primeiro componente em React</h1>
+        </div>
+    );
+}
+
+export default Titulo;
